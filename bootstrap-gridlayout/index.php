d2fde2b2ca7b696600b028a04d84ddfa84d2fcaa
kungunathan <kungun78@gmail.com>
2022-06-21 12:21:58 +0300

    First commit

--- bootstrap-gridlayout/index.php
+++ bootstrap-gridlayout/index.php
@@ -5,12 +5,7 @@
  $database = "web2";
  
  $conn = mysqli_connect( $server,$username,$password,$database);
- if($conn){
-    echo "Connection successful.";
- }
- else{
-    echo "Connection unsuccessful.";
- }
+
  
  
  
